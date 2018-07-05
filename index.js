aa58ddf6abe37d9803d84a7930d3138942479a2f
za-xielingjuan <za-xielingjuan@zhongan.io>
2018-07-05 15:46:02 +0800

    feat: update

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 module.exports = {
-  root: true,
+    root: true,
 
-  parser: 'babel-eslint',
+    parser: 'babel-eslint',
 
-  env: {
-    es6: true,
-    browser: true,
-    node: true
-  },
+    env: {
+        es6: true,
+        browser: true,
+        node: true
+    },
 
-  extends: [
-    'eslint:recommended',
-    'standard'
-  ]
-}
+    extends: [
+        'eslint:recommended',
+        'standard'
+    ],
+
+    rules: {
+
+        'indent': ['error', 4],
+        'semi': ['error', 'always'], // 【强制】分号结尾
+        'space-before-function-paren': ['error', 'never'], // 【强制】function左扩号前不加空格
+        'no-console': 0, // 【无要求】对是否有console无要求。
+        'comma-dangle': 0, // 【无要求】对末尾是否能有逗号无要求。
+        'no-useless-constructor': 0,
+        'import/no-webpack-loader-syntax': 0,
+        'no-extend-native': 0, // 可扩展原生类型
+        'camelcase': 0, // 可以不是驼峰
+
+    }
+};
--- react.js
+++ react.js
@@ -1,8 +1,12 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  extends: [
-    path.join(__dirname, 'index.js'),
-    'standard-react'
-  ]
-}
+    extends: [
+        path.join(__dirname, 'index.js'),
+        'standard-react'
+    ],
+    rules: {
+        'react/jsx-indent': ['error', 4], // 【强制】jsx中dom层级药按照4 spaces的规范
+        'react/jsx-indent-props': 0,
+    }
+};
